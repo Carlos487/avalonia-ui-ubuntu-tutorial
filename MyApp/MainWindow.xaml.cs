547b67a7dc1af053c4a218c93761ac2a012424c6
Carlos Fabara <carlos487@live.com>
2020-07-24 12:12:31 -0500

    02 Interacting with controls

--- MyApp/MainWindow.xaml.cs
+++ MyApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity; //Esto se agrego
 using Avalonia.Markup.Xaml;
 
 namespace MyApp
@@ -14,6 +15,16 @@ namespace MyApp
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        public void GreetButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Getting Controls references
+            var nameControl = this.FindControl<TextBox>("NameTextBox");
+            var messageControl = this.FindControl<TextBlock>("MessageLabel");
+
+            //Setting the value
+            messageControl.Text = $"Hello {nameControl.Text} !!!";
         }
     }
 }
